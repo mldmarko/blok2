749801954ef3a104c4a51edfcaf1e5000347bf60
djordjepekic <djordjepekich@yahoo.co.uk>
2017-11-20 21:04:30 +0100

    Kreiran SecurityManager class library. Sadrzi Audit klasu koja sluzi za logovanje.

---
+++ zad2/SecurityManager/Audit.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityManager
+{
+    //Nudi funkcionalnost logovanja u pozivanjem neke od metoda
+    /*
+     * Primer poziva loga na serveru:
+     * 
+     *      Audit.AuthorizationSuccess(principal.Identity.Name, OperationContext.Current.IncomingMessageHeaders.Action);
+     * 
+     * */
+    public class Audit : IDisposable
+    {
+
+        private static EventLog customLog = null;
+        const string SourceName = "SecurityManager.Audit";
+        const string LogName = "BLOK2 Projekat - Custom Log";
+
+        static Audit()
+        {
+            try
+            {
+                if (!EventLog.SourceExists(SourceName))
+                {
+                    EventLog.CreateEventSource(SourceName, LogName);
+                }
+
+                customLog = new EventLog(LogName, Environment.MachineName, SourceName);
+            }
+            catch (Exception e)
+            {
+                customLog = null;
+                Console.WriteLine("Error while trying to create log handle. Error = {0}", e.Message);
+            }
+        }
+
+
+        public static void AuthenticationSuccess(string userName)
+        {
+            if (customLog != null)
+            {
+                string message = String.Format(AuditEventsFile.UserAuthenticationSuccess, userName);
+                customLog.WriteEntry(message);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event (eventid = {0}) to event log.", (int)AuditEventTypes.UserAuthenticationSuccess));
+            }
+        }
+
+        public static void AuthorizationSuccess(string userName, string serviceName)
+        {
+            if (customLog != null)
+            {
+                string message = String.Format(AuditEventsFile.UserAuthorizationSuccess, userName, serviceName);
+                customLog.WriteEntry(message);
+
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event (eventid = {0}) to event log.", (int)AuditEventTypes.UserAuthenticationSuccess));
+            }
+        }
+
+        public static void AuthorizationFailed(string userName, string serviceName, string reason)
+        {
+            if (customLog != null)
+            {
+                string message = String.Format(AuditEventsFile.UserAuthorizationFailed, userName, serviceName, reason);
+                customLog.WriteEntry(message);
+
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event (eventid = {0}) to event log.", (int)AuditEventTypes.UserAuthenticationSuccess));
+            }
+        }
+
+        public static void AuthenticationFailed(string userName)
+        {
+            if (customLog != null)
+            {
+                string message = String.Format(AuditEventsFile.UserAuthenticationFailed, userName);
+                customLog.WriteEntry(message);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event (eventid = {0}) to event log.", (int)AuditEventTypes.UserAuthenticationSuccess));
+            }
+        }
+
+        public void Dispose()
+        {
+            if (customLog != null)
+            {
+                customLog.Dispose();
+                customLog = null;
+            }
+        }
+    }
+}
---
+++ zad2/SecurityManager/AuditEventTypes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityManager
+{
+    public enum AuditEventTypes
+    {
+        UserAuthenticationSuccess = 0,
+        UserAuthorizationSuccess = 1,
+        UserAuthorizationFailed = 2,
+        UserAuthenticationFailed = 3
+    }
+
+    //Definise i vraca tipove dogadjaja koje je moguce logovati
+    //Tipovi dogadjaja su definisani u AuditEventTypes.resx resursnom fajlu
+    public class AuditEvents
+    {
+        private static ResourceManager resourceManager = null;
+        private static object resourceLock = new object();
+
+        private static ResourceManager ResourceMgr
+        {
+            get
+            {
+                lock (resourceLock)
+                {
+                    if (resourceManager == null)
+                    {
+                        resourceManager = new ResourceManager(typeof(AuditEventsFile).FullName, Assembly.GetExecutingAssembly());
+                    }
+                    return resourceManager;
+                }
+            }
+        }
+
+        public static string UserAuthenticationSuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.UserAuthenticationSuccess.ToString());
+            }
+        }
+
+        public static string UserAuthorizationSuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.UserAuthorizationSuccess.ToString());
+            }
+        }
+
+        public static string UserAuthorizationFailed
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.UserAuthorizationFailed.ToString());
+            }
+        }
+
+        public static string UserAuthenticationFailed
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.UserAuthenticationFailed.ToString());
+            }
+        }
+    }
+}
